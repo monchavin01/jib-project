205e449b549eb30f249aec7bd173151814aaf37d
monchavin01 <monchavin.pi@gmail.com>
2020-08-06 14:23:38 +0700

    create list_filter on test in test.py

--- safety/workers/admin.py
+++ safety/workers/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Worker
 
 @admin.register(Worker)
-class WorkAdmin(admin.ModelAdmin):
+class WorkerAdmin(admin.ModelAdmin):
+
+  
     list_display = (
         'first_name',
         'last_name',
--- safety/workers/tests.py
+++ safety/workers/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-
-
 from .models import Worker
+from django.contrib import admin
+from .admin import WorkerAdmin
+
 
 class TestWorker(TestCase):
     def test_worker_should_have_defined_fields(self):
@@ -29,3 +30,27 @@ class TestWorker(TestCase):
         self.assertEqual(worker.primary_phone, primary_phone)
         self.assertEqual(worker.secondary_phone, secondary_phone)
         self.assertEqual(worker.address, address)
+
+
+
+
+
+class WorkerAdninTest(TestCase):
+    def test_admin_should_be_registered(self):
+        self.assertTrue(isinstance(admin.site._registry[Worker], WorkerAdmin))
+
+    def test_admin_should_set_list_display(self):
+        expected = (
+             'first_name',
+            'last_name',
+            'is_available',
+            'primary_phone',
+            'secondary_phone',
+        )
+        self.assertEqual(WorkerAdmin.list_display, expected)
+
+    def test_admin_should_set_list_filter(self):
+        expected = (
+              'is_available',
+        )
+        self.assertEqual(WorkerAdmin.list_filter, expected)
