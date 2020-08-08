6a67107e3f3bf2b936f5314728d0a09934f3b4cc
monchavin01 <monchavin.pi@gmail.com>
2020-08-08 15:15:11 +0700

    Edid file certificate

---
+++ safety/certificate/tests/test_admin.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from ..admin import CertificateAdmin
+from django.test import TestCase
+from ..models import Certificate
+
+
+class CertificateAdnimTest(TestCase):
+    def test_admin_should_be_registered(self):
+        assert isinstance(admin.site._registry[Certificate], 
+                         CertificateAdmin) is True
+
+    def test_admin_should_set_list_display(self):
+        expected = (
+            'name',
+            'issued_by',
+            
+        )
+        assert CertificateAdmin.list_display == expected
+
+   
