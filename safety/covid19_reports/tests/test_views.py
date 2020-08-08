78e94277590ea1f4d42a86ff5d0cf569e36ef8d5
monchavin01 <monchavin.pi@gmail.com>
2020-08-08 11:21:20 +0700

    add @patch('covid19_reports.views.requests.get')on function top

--- safety/covid19_reports/tests/test_views.py
+++ safety/covid19_reports/tests/test_views.py
@@ -3,7 +3,9 @@ from unittest.mock import patch
 # Create your tests here.
 
 class TestCovid19ReportView(TestCase):
-    def test_view_should_be_accessible(self):
+
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_be_accessible(self, _):
         response = self.client.get('/covid19-reports/')
         self.assertEqual(response.status_code, 200)
 
