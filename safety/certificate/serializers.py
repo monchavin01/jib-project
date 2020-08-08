971c858a71a42f9461e584254d9db95d677ecc09
monchavin01 <monchavin.pi@gmail.com>
2020-08-08 15:26:08 +0700

    Create files test_serializer.py and serializer.py

---
+++ safety/certificate/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Certificate
+
+
+class CertificateModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = Certificate
---
+++ safety/certificate/tests/test_serializers.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from ..serializers import CertificateModelSerializer
+from ..models import Certificate
+
+class TestCertificateModelSerializer(TestCase):
+    def test_model_serializer_should_set_certificate_model(self):
+        
+        assert CertificateModelSerializer.Meta.model == Certificate
