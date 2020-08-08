f5167a83c7b770304c9df41eff5f6c229e4339f7
monchavin01 <monchavin.pi@gmail.com>
2020-08-08 11:13:28 +0700

    Add function Test_view... in file test_views.py

--- safety/covid19_reports/tests/test_views.py
+++ safety/covid19_reports/tests/test_views.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
-
+from unittest.mock import patch
 # Create your tests here.
 
 class TestCovid19ReportView(TestCase):
     def test_view_should_be_accessible(self):
         response = self.client.get('/covid19-reports/')
         self.assertEqual(response.status_code, 200)
+
+ # new add
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_call_covid19_api(self, mock):
+        self.client.get('/covid19-reports/')
+        mock.assert_called_once_with(
+            'https://covid19.th-stat.com/api/open/today'
+        )
--- safety/covid19_reports/views.py
+++ safety/covid19_reports/views.py
@@ -1,8 +1,11 @@
 from django.views import View
 from django.http import HttpResponse
 
+import requests
+
 class Covid19ReportView(View):
     def get(self, request):
+        requests.get('https://covid19.th-stat.com/api/open/today')
         return HttpResponse()
 
 
